0869ddae8c3873411b06bf48b1223ffe492ba69a
Aurel <aurel.kilian@aol.com>
2017-05-18 14:59:11 +0200

    bla

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const port = 8080;
+
+
 var onRequest = function(req, res){
   console.log(req.method+': '+req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -53,7 +55,3 @@ function dbConnection() {
 		});		
 	}
 	
-	
-
-
-
