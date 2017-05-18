68bede9a8e4933e41d2cfe6971da448f26a1ba1e
Aurel <aurel.kilian@aol.com>
2017-05-18 13:25:34 +0200

    changed script

--- hello.js
+++ hello.js
@@ -1,14 +1,10 @@
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+const http = require('http');
+const port = 8080;
+var onRequest = function(req, res){
+  console.log(req.method+': '+req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello Team Uniform!');
+  res.end();
+}
+http.createServer(onRequest).listen(port);
+console.log('Server listening on port '+ port);
