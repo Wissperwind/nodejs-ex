dd60756861e588b96a5f563ba798e55ce21d31d8
Sven Dotzauer-Klier <Sven Dotzauer-Klier>
2017-06-01 02:34:14 +0200

    included contents of testServer.js into server.js

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-restify = require('restify');
-
+const restify = require('restify');
+const db = require('./database');
+const login = require('./login');
 
 const port = 8080;
 
 server = restify.createServer();
-server.listen(port);
+
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
+
 server.on('uncaughtException', function (req, res, route, err) {
     console.log('uncaughtException', err.stack);
 });
-
-var login = require('./login');
 
 server.post('/signup', login.signUp);
 
 server.post({url:'/login'}, login.loginRoute);
 server.get({url:'/hello'}, login.helloRoute);
 
+server.get('venues/position/:latlng',	db.getVenues); // latlng should be "lat,lng", split did not work with #
+server.get('venues/:id',				db.getVenue);
 
-
-
-
-server.listen(3000, function(){
+/* server.listen(3000, function(){
     console.log('%s is listening at %s', server.name, server.url);
+}); */
+
+server.listen(port, function(){
+	console.log('%s is listening at %s : %s', server.name, server.url, port);
+	db.connect();
 });
