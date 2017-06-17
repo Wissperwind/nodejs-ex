eab3a0ab541a145e4f16e440b9105b1dce009397
Sven Dotzauer-Klier <Sven Dotzauer-Klier>
2017-06-17 22:57:06 +0200

    try again

--- photoModule.js
+++ photoModule.js
@@ -6,8 +6,8 @@ function photoModule(){
 	var fs = require('fs');
 	
 	//that.photoDir = process.env.OPENSHIFT_DATA_DIR; //does not work...
-	that.photoDir = fs.readdirSync("");
-	console.log("data folder: "+fs.readdirSync("/data"));
+	that.photoDir = fs.existsSync("");
+	console.log("data folder: "+fs.existsSync("/data"));
 }
 
 module.exports = new photoModule();
