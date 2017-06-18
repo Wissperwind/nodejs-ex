62b825758d38c08cbe4eed180a80c57ff7c3cb00
Sven Dotzauer-Klier <Sven Dotzauer-Klier>
2017-06-18 15:28:56 +0200

    try again

--- photoModule.js
+++ photoModule.js
@@ -7,8 +7,8 @@ function photoModule(){
 	
 	//that.photoDir = process.env.OPENSHIFT_DATA_DIR; //does not work...
 	that.photoDir = process.env.PWD.replace("src", "data");
-	console.log(photoDir);
-	console.log("data folder exists: "+fs.existsSync(photoDir));
+	console.log(that.photoDir);
+	console.log("data folder exists: "+fs.existsSync(that.photoDir));
 }
 
 module.exports = new photoModule();
