ec007e3c63d36cfb962a06c0b981e29495f33033
Faaz <faaz.iqbal@gmail.com>
2017-07-29 17:56:33 +0200

    Password reset functionality for logged in and logged out users

--- server.js
+++ server.js
@@ -38,11 +38,21 @@ server.post({url:'/login'}, authModule.logIn);
 //use authModule.ensureAuthenticated() for each request requiring authentication
 // in order to return the error when user is not authenticated
 server.get({url:'/hello'}, authModule.ensureAuthenticated, authModule.helloRoute);
+server.get({url:'/logout'}, authModule.ensureAuthenticated, function (req, res, next) {
+    req.session.destroy();
+    res.json({
+        "error": 'false'
+    });
+    return next();
+});
 server.get({url:'/account'}, authModule.ensureAuthenticated, userModule.getUserInfo);
 server.put({url:'/account'}, authModule.ensureAuthenticated, userModule.updateUserInfo);
 server.del({url:'/account'}, authModule.ensureAuthenticated, userModule.deleteUser);
-server.put({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.resetPassword);
 server.post({url:'/account/profilepicture'}, authModule.ensureAuthenticated, photoModule.postPhotoUser);
+server.get({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.getPasswordResetToken); //For logged out users
+server.put({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.updatePassword); //For logged out users
+server.get({url:'/pw'}, userModule.getPasswordResetToken); //For logged in users
+server.put({url:'/pw'}, userModule.updatePassword); //For logged in users
 
 
 // should we reconsider the URLs? maybe:
