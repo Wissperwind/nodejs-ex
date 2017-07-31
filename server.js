225bbadec77f07ae0f7dfdcb9ce9b5661ca0c315
Sven Dotzauer-Klier <sven.dk@web.de>
2017-07-31 17:25:29 +0200

    server.js corrections and cleanup

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ var friendModule = require('./friendModule');
 var commentModule = require('./commentModule');
 var photoModule = require('./photoModule');
 var chat = require('./chatModule');
+var highscoreModule = require('./highscoreModule');
 
 const port = 8080;
 
 server = restify.createServer({
 	name: 'IPTKServerTeamUniform',
 });
-//server.listen(port)
-
 
 server.use(plugins.bodyParser());
 server.use(plugins.queryParser({mapParams: true}));
@@ -40,37 +39,33 @@ server.post({url:'/login'}, authModule.logIn);
 //use authModule.ensureAuthenticated() for each request requiring authentication
 // in order to return the error when user is not authenticated
 server.get({url:'/hello'}, authModule.ensureAuthenticated, authModule.helloRoute);
+
 server.get({url:'/account'}, authModule.ensureAuthenticated, userModule.getUserInfo);
 server.put({url:'/account'}, authModule.ensureAuthenticated, userModule.updateUserInfo);
 server.del({url:'/account'}, authModule.ensureAuthenticated, userModule.deleteUser);
+
 server.post({url:'/friends'},authModule.ensureAuthenticated, friendModule.postfriend);
 server.get({url:'/friends'},authModule.ensureAuthenticated, friendModule.getUserfriend);
- server.post({url:'/chat'}, authModule.ensureAuthenticated,chat.postchat);
- server.get({url:'/chat'},authModule.ensureAuthenticated, chat.getchat);
 server.del({url:'/friends'},authModule.ensureAuthenticated, friendModule.deleteUser);
+
+server.post({url:'/chat'}, authModule.ensureAuthenticated,chat.postchat);
+server.get({url:'/chat'},authModule.ensureAuthenticated, chat.getchat);
 server.get({url:'/friendprofile'},authModule.ensureAuthenticated, chat.getUserInfo);
-server.put({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.resetPassword);
+
 server.post({url:'/account/profilepicture'}, authModule.ensureAuthenticated, photoModule.postPhotoUser);
 
+server.get({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.getPasswordResetToken); //For logged out users
+server.put({url:'/pwreset'}, authModule.ensureAuthenticated, userModule.updatePassword); //For logged out users
+server.get({url:'/pw'}, userModule.getPasswordResetToken); //For logged in users
+server.put({url:'/pw'}, userModule.updatePassword); //For logged in users
 
-// should we reconsider the URLs? maybe:
-// /users
-// /venues
-// /venues/<venueID>/comments/<userID>
-// /venues/<venueID>/ratings/<userID>
-// ...
+server.get({url:'/highscorelist'}, highscoreModule.getList);
 
-
-
-//server.get('venues/position/:latlng',	venueModule.getVenues); // deprecated; latlng should be "lat,lng", split did not work with #
-//server.get('venues',					venueModule.getVenues); // expected: venues?lat=<...>&lng=<...> or venues?city=...
-server.get('venues',		venueModule.getVenues); //expected object keys: lat:..., lng:... | city:...
+server.get('venues',		venueModule.getVenues);
 server.get('venues/:id',	venueModule.getVenue);
 
 server.post('checkin',	venueModule.checkIn);
 
-//server.post('venueRatings',		venueModule.rateVenue);
-//server.get('venueRatings',	venueModule.getRatingForUser);
 server.get('venues/:id/ratings',venueModule.getRatingForUser);
 
 server.post('comments',		commentModule.postComment); //comment:..., venueid:...
@@ -82,16 +77,7 @@ server.get('photos/:id',			photoModule.getPhoto);
 
 server.put('location',	userModule.putPosition);
 
-//server.post('commentRatings',	commentModule.rateComment); //comment:..., rating:... (-1 or +1)
-//server.put('commentRatings/:id',commentModule.rateComment);
-
-/* server.listen(3000, function(){
-    console.log('%s is listening at %s', server.name, server.url);
-}); */
-
 server.listen(port, function(){
 	console.log('%s is listening at %s', server.name, server.url);
-	//console.log('Photos are in %s', photoModule.photoDir);
-	//console.log(process.env);
 	database.connect();
 });
